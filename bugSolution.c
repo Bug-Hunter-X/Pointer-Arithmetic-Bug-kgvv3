69e1ffd02d67d3ff9314402d1e80dd6f27e866c7
BugHunter-X <askzenai@gmail.com>
2024-12-22 17:32:03 +0100

    Add bugSolution.c file

---
+++ bugSolution.c
@@ -0,0 +1,1 @@
+No changes needed to the code, it is working as expected. The bug is related to understanding how pointers work. Modifying the value at the memory address pointed to by the pointer modifies the original variable.
